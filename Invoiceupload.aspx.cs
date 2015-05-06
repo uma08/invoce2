071232dfef99128e00e1afc49996a7a319803a89
umagayathri08 <umagayathri08@gmail.com>
2015-05-07 00:09:20 +0530

    invoice2

---
+++ Invoiceupload.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+
+namespace invoice
+{
+    public partial class Invoiceupload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnImport_Click1(object sender, EventArgs e)
+        {
+            string connString = "";
+            string strFileType = Path.GetExtension(fileuploadExcel.FileName).ToLower();
+            string path = fileuploadExcel.PostedFile.FileName;
+            //Connection String to Excel Workbook
+            if (strFileType.Trim() == ".xls")
+            {
+                connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
+            }
+            else if (strFileType.Trim() == ".xlsx")
+            {
+                connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+            }
+            string query = "SELECT [Username],[Education],[Location] FROM [Sheet1$]";
+            OleDbConnection conn = new OleDbConnection(connString);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            OleDbCommand cmd = new OleDbCommand(query, conn);
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            grvExcelData.DataSource = ds.Tables[0];
+            grvExcelData.DataBind();
+            da.Dispose();
+            conn.Close();
+            conn.Dispose();
+        }
+
+
+    }
+}
+    
